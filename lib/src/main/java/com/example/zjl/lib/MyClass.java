f4463db4910733e7f44839debce5b5d1cef84e2a
zjl7577 <1097919195@qq.com>
2018-09-14 10:50:35 +0800

    java 获取键盘输入“

--- lib/src/main/java/com/example/zjl/lib/MyClass.java
+++ lib/src/main/java/com/example/zjl/lib/MyClass.java
@@ -1,6 +1,7 @@
 package com.example.zjl.lib;
 
 import java.lang.ref.WeakReference;
+import java.util.Scanner;
 
 public class MyClass {
     //声明：对异常不处理，收到异常我也向外抛
@@ -35,6 +36,20 @@ public class MyClass {
         System.out.println(sr.get());
         System.gc();                //通知JVM的gc进行垃圾回收
         System.out.println(sr.get());
+
+
+
+        Scanner scan = new Scanner(System.in);
+        // 从键盘接收数据
+
+        // next方式接收字符串
+        System.out.println("next方式接收：");
+        // 判断是否还有输入
+        if (scan.hasNextLine()) {
+            String str1 = scan.next();
+            System.out.println("输入的数据为：" + str1);
+        }
+        scan.close();
 
     }
 
