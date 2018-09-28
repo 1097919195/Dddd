1d5a61a06e35036a4bd87d61b9eb7a9d3535e1ae
zjl7577 <1097919195@qq.com>
2018-09-28 11:05:50 +0800

    temp

--- app/src/main/java/com/example/zjl/dddd/activity/TestActivity.java
+++ app/src/main/java/com/example/zjl/dddd/activity/TestActivity.java
@@ -140,13 +140,13 @@ public class TestActivity extends AppCompatActivity {
     public void updateApp(View view) {
         UpdateAppUtils.from(this)
                 .checkBy(UpdateAppUtils.CHECK_BY_VERSION_NAME) //更新检测方式，默认为VersionCode
-                .serverVersionCode(1)
+                .serverVersionCode(2)
                 .serverVersionName("2.0")//要大于1.0
                 .apkPath("https://www.npclo.com/dlo")
-                .showNotification(false) //是否显示下载进度到通知栏，默认为true
+                .showNotification(true) //是否显示下载进度到通知栏，默认为true
                 .updateInfo("更新更新")  //更新日志信息 String
-                .downloadBy(UpdateAppUtils.DOWNLOAD_BY_BROWSER) //下载方式：app下载、手机浏览器下载。默认app下载
-                .isForce(true) //是否强制更新，默认false 强制更新情况下用户不同意更新则不能使用app
+                .downloadBy(UpdateAppUtils.DOWNLOAD_BY_APP) //下载方式：app下载、手机浏览器下载。默认app下载
+                .isForce(false) //是否强制更新，默认false 强制更新情况下用户不同意更新则不能使用app
                 .update();
     }
 
