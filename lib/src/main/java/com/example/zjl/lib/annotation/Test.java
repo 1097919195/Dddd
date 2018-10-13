e24eb0454dad1de6b7951fefcc362ded57433ac9
zjl7577 <1097919195@qq.com>
2018-10-13 09:42:12 +0800

    MessageDigest

--- lib/src/main/java/com/example/zjl/lib/annotation/Test.java
+++ lib/src/main/java/com/example/zjl/lib/annotation/Test.java
@@ -3,6 +3,8 @@ package com.example.zjl.lib.annotation;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -16,6 +18,8 @@ public class Test {
         List<Integer> useCases = new ArrayList<Integer>();
         Collections.addAll(useCases, 47, 48, 49, 50);
         trackUseCases(useCases, PasswordUtils.class);
+
+        System.err.println(stringMD5("6572BDAFF799084B973320F43F09B363"));
     }
 
     public static void trackUseCases(List<Integer> useCases, Class<?> cl) {
@@ -30,6 +34,39 @@ public class Test {
         for (int i : useCases) {
             System.out.println("Warning: Missing use case -- " + i);
         }
+    }
+
+    //Java利用MessageDigest获取字符串或文件MD5详解
+    public static String stringMD5(String input) {
+        try {
+            // 拿到一个MD5转换器（如果想要SHA1参数换成”SHA1”）
+            MessageDigest messageDigest =MessageDigest.getInstance("MD5");
+            // 输入的字符串转换成字节数组
+            byte[] inputByteArray = input.getBytes();
+            // inputByteArray是输入字符串转换得到的字节数组
+            messageDigest.update(inputByteArray);
+            // 转换并返回结果，也是字节数组，包含16个元素
+            byte[] resultByteArray = messageDigest.digest();
+            // 字符数组转换成字符串返回
+            return byteArrayToHex(resultByteArray);
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        }
+    }
+
+    public static String byteArrayToHex(byte[] byteArray) {
+        // 首先初始化一个字符数组，用来存放每个16进制字符
+        char[] hexDigits = {'0','1','2','3','4','5','6','7','8','9', 'A','B','C','D','E','F' };
+        // new一个字符数组，这个就是用来组成结果字符串的（解释一下：一个byte是八位二进制，也就是2位十六进制字符（2的8次方等于16的2次方））
+        char[] resultCharArray =new char[byteArray.length * 2];
+        // 遍历字节数组，通过位运算（位运算效率高），转换成字符放到字符数组中去
+        int index = 0;
+        for (byte b : byteArray) {
+            resultCharArray[index++] = hexDigits[b>>> 4 & 0xf];
+            resultCharArray[index++] = hexDigits[b& 0xf];
+        }
+        // 字符数组组合成字符串返回
+        return new String(resultCharArray);
     }
 
 }
